09e4192adf34f609a2b0fd6df68d432aa98891aa
Ryan Hoo <94471798+Cluix1@users.noreply.github.com>
2026-09-01 13:39:50 -0600

    added 1 activity with 2 fragments for in-class 1

--- In-Class 1/EmailSplitter/app/src/main/java/com/example/emailsplitter/MainActivity.kt
+++ In-Class 1/EmailSplitter/app/src/main/java/com/example/emailsplitter/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.emailsplitter
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.emailsplitter.databinding.ActivityMainBinding
 
@@ -13,27 +12,5 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener {
-
-            val email = binding.emailInput.text.toString()
-            val pieces = email.split('@')
-
-            if (pieces.size != 2 || pieces.any(String::isEmpty)) {
-
-                Toast.makeText(
-                    this,
-                    "Invalid email!",
-                    Toast.LENGTH_SHORT
-                ).show()
-
-            } else {
-
-                val username = pieces[0]
-                val domain = pieces[1]
-
-                binding.userView.text = username
-                binding.domainView.text = domain
-            }
-        }
     }
 }
